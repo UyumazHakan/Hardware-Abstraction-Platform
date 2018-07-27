a5c558077406b73773e0748038501ca6660e304c
ge69let <ge69let@mytum.de>
2018-07-27 16:46:23 +0200

    Updated device_enum.py

--- device_enum.py
+++ device_enum.py
@@ -23,7 +23,7 @@ device_constructors ={
 	36 : KY_36,
 	37 : KY_37,
 	50 : KY_50,
-	51 : WEATHER2_BOARD,
+	51 : WEATHER2BOARD,
         52 : KY_52,
 	53 : MYAHRS_PLUS
 }
@@ -79,6 +79,6 @@ class DeviceEnum(Enum):
 	KY48 = 48
 	KY49 = 49
 	KY50 = 50
-	WEATHER2_BOARD = 51
+	WEATHER2BOARD = 51
 	KY52 = 52
 	MYAHRS_PLUS = 53
