1a4942f749ee6528831dc4368c4afcc7a66b2a89
ga84doy <ga84doy@mytum.de>
2018-07-28 15:16:13 +0200

    bug fix

--- communication_manager.py
+++ communication_manager.py
@@ -27,9 +27,6 @@ class CommunicationManager:
 
 
 	def send_all(self, data, callback = None):
-		print("communication manager: send_all")
-		print(data)
-		print('\n\n')
 		if not callback:
 			callback = self.send_callback
 		for protocol_id in self.communication_protocols:
--- security_communication/communication/http_communication_protocol.py
+++ security_communication/communication/http_communication_protocol.py
@@ -12,7 +12,6 @@ import requests
 class HTTPCommunicationProtocol(CommunicationProtocol):
 
 	def __init__(self, config, send_callback = None, receive_callback = None):
-		#print(config)
 		super(HTTPCommunicationProtocol, self).__init__(config, send_callback, receive_callback)
 		self.servers = self.config["bootstrap_servers"]
 		self.topic = self.config["topic"]
@@ -21,14 +20,14 @@ class HTTPCommunicationProtocol(CommunicationProtocol):
 	def _send_to_single_server(self, server, data):
 		try:
 			headers = {"Authorization":"Bearer "+server['password']}
-			print(data)
 			msg = {}
 			msg["timestamp"] = data["msg"]["timestamp"] * 1000
 			msg["sensor_id"] = data["msg"]["custom_id"]
 			msg["value"] = data["msg"]["values"]
 			print(msg)
 			url = server['ip_address']+":"+server['port']
-			print(requests.post(url, data=json.dumps(msg), headers=headers))
+			response = requests.post(url, data=json.dumps(msg), headers=headers)
+			print(response)
 
 		except Exception as e:
 			print(e)
@@ -36,13 +35,10 @@ class HTTPCommunicationProtocol(CommunicationProtocol):
 
 
 	def send(self, data, callback = None):
-		print("inside http send")
-		print(data)
 		if not callback:
 		    callback = self.send_callback
 
 		for server in self.servers:
-			print(server)
 			self._send_to_single_server(server, data)
 
 		if callback:
@@ -50,7 +46,3 @@ class HTTPCommunicationProtocol(CommunicationProtocol):
 
 	def receive(self, callback = None):
 		pass
-
-
-	def random_string (self, length):
-		return ''.join (random.choice(string.ascii_letters) for ii in range(length + 1))
